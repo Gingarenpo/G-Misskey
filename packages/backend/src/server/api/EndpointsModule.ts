7f24d0197f78a876f59b31cfb143590d7714e729
Gingarenpo <gingarenpo.9844@gmail.com>
2024-06-17 21:40:18 +0900

    midi_kakiko Add

--- packages/backend/src/server/api/EndpointsModule.ts
+++ packages/backend/src/server/api/EndpointsModule.ts
@@ -7,12 +7,18 @@ import { Module } from '@nestjs/common';
 
 import { CoreModule } from '@/core/CoreModule.js';
 import * as endpointsObject from './endpoint-list.js';
+
+import * as ep___midi_kakiko from './endpoints/midi_kakiko/midi_kakiko.js';
+
+
 import { GetterService } from './GetterService.js';
 import { ApiLoggerService } from './ApiLoggerService.js';
 import type { Provider } from '@nestjs/common';
 
 const endpoints = Object.entries(endpointsObject);
 const endpointProviders = endpoints.map(([path, endpoint]): Provider => ({ provide: `ep:${path}`, useClass: endpoint.default }));
+
+const $midi_kakiko: Provider = { provide: 'ep:midi_kakiko', useClass: ep___midi_kakiko.default };
 
 @Module({
 	imports: [
@@ -22,9 +28,14 @@ const endpointProviders = endpoints.map(([path, endpoint]): Provider => ({ provi
 		GetterService,
 		ApiLoggerService,
 		...endpointProviders,
+
+		$midi_kakiko,
+		KakikoService,
 	],
 	exports: [
 		...endpointProviders,
+
+		$midi_kakiko,
 	],
 })
 export class EndpointsModule {}
