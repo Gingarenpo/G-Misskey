886e43b1f0f3dfdb70f092a70c26dfdd7f7411b3
Gingarenpo <gingarenpo.9844@gmail.com>
2024-07-07 00:50:43 +0900

    Add time display "RelativeAndAbsolute"

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -792,6 +792,14 @@ function isAnnoying(text: string): boolean {
 		text.includes('$[position');
 }
 
+function isAnnoying(text: string): boolean {
+	return text.includes('$[x2') ||
+		text.includes('$[x3') ||
+		text.includes('$[x4') ||
+		text.includes('$[scale') ||
+		text.includes('$[position');
+}
+
 async function post(ev?: MouseEvent) {
 	if (useCw.value && (cw.value == null || cw.value.trim() === '')) {
 		os.alert({
--- packages/frontend/src/pages/timeline.vue
+++ packages/frontend/src/pages/timeline.vue
@@ -12,8 +12,8 @@ SPDX-License-Identifier: AGPL-3.0-only
 				{{ i18n.ts._timelineDescription[src] }}
 			</MkInfo>
 			<MkPostForm v-if="prefer.r.showFixedPostForm.value" :class="$style.postForm" class="_panel" fixed style="margin-bottom: var(--margin);"/>
-				<!--<midi_kakiko>
-				</midi_kakiko>-->
+				<!--<!--<midi_kakiko>
+				</midi_kakiko>-->-->
 				
 			<div v-if="queue > 0" :class="$style.new"><button class="_buttonPrimary" :class="$style.newButton" @click="top()">{{ i18n.ts.newNoteRecived }}</button></div>
 			<MkTimeline
