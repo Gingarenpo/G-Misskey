62af5138713cbde6b808035655647f1abe011b18
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-10-20 17:29:41 +0900

    fix(frontend): 通知の範囲指定が必要ない通知設定でも範囲指定がでている問題を修正 (#14798)
    * fix(frontend): 通知の範囲指定が必要ない通知設定でも範囲指定がでている問題を修正
    
    * Update Changelog
    
    ---------
    
    Co-authored-by: syuilo <4439005+syuilo@users.noreply.github.com>

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -9285,7 +9285,7 @@ export interface Locale extends ILocale {
          */
         "youGotQuote": ParameterizedString<"name">;
         /**
-         * {name}がRenoteしました
+         * {name}がリノートしました
          */
         "youRenoted": ParameterizedString<"name">;
         /**
@@ -9390,7 +9390,7 @@ export interface Locale extends ILocale {
              */
             "reply": string;
             /**
-             * Renote
+             * リノート
              */
             "renote": string;
             /**
@@ -9448,7 +9448,7 @@ export interface Locale extends ILocale {
              */
             "reply": string;
             /**
-             * Renote
+             * リノート
              */
             "renote": string;
         };
