d5c4120a703ed9c54b8a1c9f02b4493244aad207
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-03-10 18:35:51 +0900

    enhance(frontend): テーマ設定で簡易プレビューを表示するように (#15643)
    * enhance(frontend): テーマ設定で簡易プレビューを表示するように
    
    * Update Changelog
    
    * fix lint
    
    * 🎨
    
    ---------
    
    Co-authored-by: syuilo <4439005+syuilo@users.noreply.github.com>

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -7761,6 +7761,10 @@ export interface Locale extends ILocale {
          */
         "builtinThemes": string;
         /**
+         * サーバーのテーマ
+         */
+        "instanceTheme": string;
+        /**
          * そのテーマは既にインストールされています
          */
         "alreadyInstalled": string;
