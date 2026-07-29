df6dc83cea2884bd13991f81949f28b01b98efd2
Gingarenpo <gingarenpo.9844@gmail.com>
2026-07-29 22:30:03 +0900

    開発

--- packages/backend/src/server/api/endpoint-list.ts
+++ packages/backend/src/server/api/endpoint-list.ts
@@ -448,3 +448,5 @@ export * as 'chat/rooms/invitations/outbox' from './endpoints/chat/rooms/invitat
 export * as 'chat/history' from './endpoints/chat/history.js';
 export * as 'chat/read-all' from './endpoints/chat/read-all.js';
 export * as 'v2/admin/emoji/list' from './endpoints/v2/admin/emoji/list.js';
+
+export * as 'midi_kakiko' from './endpoints/midi_kakiko/midi_kakiko.js';
